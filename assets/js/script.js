cddcd871ad10d45459be809ec2bf845a36959c2a
Andrzej <andrzej.krzysztof.hryniewicz@gmail.com>
2024-04-26 14:06:03 +0300

    xx

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,12 @@ if (document.location.search === "?mail_status=sent") {
       msgStatus.classList.remove("success");
     }, 3000);
   }
-  if (document.location.search === "?mail_status=error") {
-    msgStatus.classList.add("error");
-    msgStatus.textContent = "Message not sent!";
+//   if (document.location.search === "?mail_status=error") {
+//     msgStatus.classList.add("error");
+//     msgStatus.textContent = "Message not sent!";
   
-    setTimeout(() => {
-      msgStatus.classList.remove("error");
-    }, 3000);
-  }
+//     setTimeout(() => {
+//       msgStatus.classList.remove("error");
+//     }, 3000);
+//   }
   
